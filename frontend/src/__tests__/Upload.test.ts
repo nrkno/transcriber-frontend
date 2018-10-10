6d8596c706ff183513578379f11b643ee6ae19cb
Andreas Schjønhaug <andreas@schjonhaug.com>
2018-10-10 17:00:25 +0200

    Jest for testing

---
+++ frontend/src/__tests__/Upload.test.ts
@@ -0,0 +1,12 @@
+// import Upload from "../components/Upload"
+// import React from "react"
+// import TestRenderer from "react-test-renderer"
+
+test("Simple sum", () => {
+  expect(3 + 5).toBe(8)
+})
+
+//test("renders correctly", () => {
+//const tree = TestRenderer.create(<Upload />).toJSON()
+// expect(tree).toMatchSnapshot()
+//})

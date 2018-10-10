8a5eb2a7c5e756a34c260b93892f7ce5fdaa731e
Andreas Schjønhaug <andreas@schjonhaug.com>
2018-10-10 13:14:03 +0200

    Moved serialize-error to dependencies and using europe-west1

--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -11,54 +11,57 @@ import { saveResult } from "./persistence"
 import { transcode } from "./transcoding"
 import { transcribe } from "./transcription"
 
-exports.transcription = functions.database.ref("/transcripts/{id}").onCreate(async (dataSnapshot, eventContext) => {
-  const id = dataSnapshot.key
+exports.transcription = functions
+  .region("europe-west1")
+  .database.ref("/transcripts/{id}")
+  .onCreate(async (dataSnapshot, eventContext) => {
+    const id = dataSnapshot.key
 
-  try {
-    const transcript = dataSnapshot.val() as ITranscription
+    try {
+      const transcript = dataSnapshot.val() as ITranscription
 
-    if (transcript === undefined) {
-      throw Error("Transcript missing")
+      if (transcript === undefined) {
+        throw Error("Transcript missing")
+      }
+
+      const languageCode = transcript.audioFile.languageCode
+
+      console.log(`Deployed 13:09 - Start transcription of id ${id} with ${languageCode} `)
+
+      // First, check if status is "analysing", otherwise, cancel
+
+      if (transcript.progress.status !== Status.Analysing) {
+        throw new Error("Transcript already processed")
+      }
+
+      // 1. Transcode
+
+      await database.updateStatus(id, Status.Transcoding)
+      const gcsUri = await transcode(id)
+
+      // 2. Transcribe
+
+      await database.updateStatus(id, Status.Transcribing)
+      const speechRecognitionResults = await transcribe(id, gcsUri, languageCode)
+
+      // 3. Save transcription
+
+      await database.updateStatus(id, Status.Saving)
+      await saveResult(speechRecognitionResults, id)
+
+      // 4. Done
+
+      await database.updateStatus(id, Status.Success)
+      console.log("End transcribing", id)
+    } catch (error) {
+      console.log("Error in main function")
+      console.error(error)
+
+      await database.errorOccured(id, error)
+
+      throw error
     }
-
-    const languageCode = transcript.audioFile.languageCode
-
-    console.log(`Deployed 15:16 - Start transcription of id ${id} with ${languageCode} `)
-
-    // First, check if status is "analysing", otherwise, cancel
-
-    if (transcript.progress.status !== Status.Analysing) {
-      throw new Error("Transcript already processed")
-    }
-
-    // 1. Transcode
-
-    await database.updateStatus(id, Status.Transcoding)
-    const gcsUri = await transcode(id)
-
-    // 2. Transcribe
-
-    await database.updateStatus(id, Status.Transcribing)
-    const speechRecognitionResults = await transcribe(id, gcsUri, languageCode)
-
-    // 3. Save transcription
-
-    await database.updateStatus(id, Status.Saving)
-    await saveResult(speechRecognitionResults, id)
-
-    // 4. Done
-
-    await database.updateStatus(id, Status.Success)
-    console.log("End transcribing", id)
-  } catch (error) {
-    console.log("Error in main function")
-    console.error(error)
-
-    await database.errorOccured(id, error)
-
-    throw error
-  }
-})
+  })
 
 process.on("unhandledRejection", (reason: any, promise: Promise<any>) => {
   console.error(new Error(`Unhandled Rejection at: Promise: ${promise} with reason: ${reason.stack || reason}`))

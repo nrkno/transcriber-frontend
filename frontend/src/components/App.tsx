c5db3ae8e755cdd719ec00805676fb2b2677c68c
Andreas Schjønhaug <andreas@schjonhaug.com>
2018-11-08 16:36:18 +0100

    Added display name to header

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -3,6 +3,7 @@ import * as React from "react"
 import ReactGA from "react-ga"
 import { BrowserRouter, Route, Switch } from "react-router-dom"
 import "../css/App.css"
+import { auth } from "../firebaseApp"
 import Transcript from "./Transcript"
 import Upload from "./Upload"
 
@@ -16,13 +17,35 @@ history.listen((location, action) => {
   ReactGA.pageview(location.pathname)
 })
 
-class App extends React.Component {
+interface IState {
+  user?: firebase.User
+}
+
+class App extends React.Component<any, IState> {
+  constructor(props: any) {
+    super(props)
+    this.state = {
+      user: undefined,
+    }
+  }
+
+  public async componentDidMount() {
+    auth.onAuthStateChanged(user => {
+      if (user) {
+        console.log("onAuthStateChanged")
+        console.log(user)
+        this.setState({ user })
+      }
+    })
+  }
+
   public render() {
     return (
       <BrowserRouter>
         <div className="container">
           <header className="org-color-dark">
             <h1 className="org-text-l">Transkribering {process.env.NODE_ENV === "development" ? "(utvikling)" : ""}</h1>
+            <div className="user">{this.state.user !== undefined ? this.state.user.displayName : ""}</div>
           </header>
           <Switch>
             <Route exact={true} path="/" component={Upload} />
--- frontend/src/firebaseApp.ts
+++ frontend/src/firebaseApp.ts
@@ -2,23 +2,20 @@
 import firebase from "firebase/app"
 
 // These imports load individual services into the firebase namespace.
-import "firebase/firestore"
-import "firebase/functions"
+import "firebase/auth"
+import "firebase/database"
 import "firebase/storage"
 
 const config = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
 }
 
 firebase.initializeApp(config)
 
-const database = firebase.firestore()
-const settings = { timestampsInSnapshots: true }
-database.settings(settings)
+const auth = firebase.auth()
+const database = firebase.database()
 const storage = firebase.storage()
-const functions = firebase.app().functions("europe-west1")
 
-export { database, storage, functions }
+export { auth, database, storage }

efe82f98e3472fe66fd87de91e0f8d25d0d12d8a
Andreas Schjønhaug <andreas@schjonhaug.com>
2018-11-22 10:12:22 +0100

    Disabled service worker

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,7 +1,7 @@
 import * as React from "react"
 import * as ReactDOM from "react-dom"
 import App from "./components/App"
-import registerServiceWorker from "./registerServiceWorker"
+// import registerServiceWorker from "./registerServiceWorker"
 
 ReactDOM.render(<App />, document.getElementById("root") as HTMLElement)
-registerServiceWorker()
+// registerServiceWorker()

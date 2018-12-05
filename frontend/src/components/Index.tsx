58e78e2fa2fe2d3a23ed46008ff925750edbd7fb
Andreas Schjønhaug <andreas@schjonhaug.com>
2018-12-05 12:34:20 +0100

    Added intro text

--- frontend/src/components/Index.tsx
+++ frontend/src/components/Index.tsx
@@ -4,23 +4,19 @@ const Index = () => {
   return (
     <main id="transcript">
       <h1>Velkommen til NRK transkribering beta!</h1>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec volutpat ligula sapien, vitae sodales neque aliquet ac. Nulla scelerisque diam vitae dapibus mollis. Ut augue lorem, congue nec sagittis sed, accumsan in augue. Curabitur
-      maximus eros id viverra vestibulum. Proin sed risus a sem volutpat tincidunt at eget leo. Pellentesque eu ornare augue. Praesent augue nisl, imperdiet ut lacinia scelerisque, tincidunt et turpis. Fusce at lobortis justo. Donec feugiat
-      euismod ligula, nec cursus lorem rhoncus at. Nulla condimentum nunc sed aliquam semper. Phasellus nec sapien eu nibh lobortis cursus. Proin vel egestas mauris. Sed in nibh eu risus iaculis condimentum. Etiam id aliquam turpis. Nullam
-      tempus consequat pharetra. Proin tristique lobortis elit, at vestibulum felis vulputate ac. Curabitur id lectus at magna lobortis porta sed vitae tortor. Ut tincidunt urna vel urna euismod, pellentesque efficitur sem euismod. Proin
-      quis leo euismod, vestibulum ante a, fringilla diam. Nunc dictum elit leo, eu condimentum tortor lobortis at. Fusce suscipit justo diam, in imperdiet arcu gravida sed. Donec sapien neque, vulputate a enim non, faucibus fermentum
-      tellus. Quisque ac aliquet ligula. In fermentum libero est, pulvinar aliquam nibh faucibus id. Mauris blandit luctus turpis. Cras massa sem, fringilla vel feugiat vitae, efficitur vel purus. Phasellus id accumsan mi. Nam aliquam orci
-      a mauris placerat, eget tincidunt metus accumsan. Integer volutpat elementum libero sed pretium. Nulla eleifend tincidunt varius. Ut nec lectus eget felis faucibus pharetra ut sit amet diam. Nulla ac laoreet mi. Vestibulum ante ipsum
-      primis in faucibus orci luctus et ultrices posuere cubilia Curae; Proin ut metus id lorem consequat aliquet. Proin non libero suscipit, sagittis est nec, fermentum lorem. Integer suscipit mi sapien, sit amet consequat augue dignissim
-      vitae. Nulla commodo commodo arcu, sit amet eleifend lacus sagittis ac. Nam tincidunt hendrerit libero nec venenatis. Nullam nunc enim, porta id venenatis ut, ullamcorper imperdiet justo. Nam dictum imperdiet magna vel cursus. Duis at
-      risus sit amet quam auctor volutpat et sed lacus. Curabitur finibus posuere augue euismod elementum. Aliquam nec egestas velit. Sed at pulvinar neque. Aenean orci enim, molestie quis leo id, feugiat varius est. Aliquam vestibulum nunc
-      eget felis venenatis rutrum. Sed viverra enim eget velit varius imperdiet. Duis tincidunt convallis viverra. Ut eleifend varius augue et viverra. Nulla vitae neque erat. Cras commodo magna a ex mattis fringilla. Sed non neque orci.
-      Phasellus dignissim tempor urna, et ultrices enim. Sed vitae augue in ex iaculis volutpat eget eu purus. Fusce lobortis euismod ipsum vitae luctus.
-      <div>
-        <button className="org-btn org-btn--primary login">
-          <a href="/login">Logg inn</a>
-        </button>
-      </div>
+      <p>
+        Dette er et verktøy som konverterer tale til tekst via Google sitt API for tale til tekst. Vi er for tiden i <a href="https://confluence.nrk.no/display/~n636391/Hva+kan+man+forvente+av+en+alfa+eller+beta+release">beta</a>, noe som
+        betyr at verktøyet er under utvikling. Det betyr også at man ikke kan forvente support, men må godta å bruke verktøyet slik det er pr. dags dato.
+      </p>
+      <p>
+        Selv om verktøyet ikke konverterer med hundre prosent nøyaktighet fra tale til tekst på alle språk, så håper vi at resultatet kan være godt nok til at du kjapt kan orientere deg i ditt eget opptak på en effektiv måte. Du kan selv
+        eksportere ut teksten til et Word-dokument og redigere i teksten som du ønsker der.
+      </p>
+      <p>Verktøyet støtter over 60 språk. Norsk språk fungerer opp mot 80%, mens engelsk opp mot 90%. Du kan laste opp alle type lydfiler, men husk at lydfilen bør være maks 200 MB, og maks lengde bør være under 1 time og 30 min.</p>
+      <p>Lykke til !</p>
+      <button className="org-btn org-btn--primary login">
+        <a href="/login">Logg inn</a>
+      </button>
     </main>
   )
 }

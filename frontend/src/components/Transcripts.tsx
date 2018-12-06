69543bef1e2386e018ac854d3dc8a392537ba121
Andreas Schjønhaug <andreas@schjonhaug.com>
2018-12-06 12:44:17 +0100

    Refactoring

--- frontend/src/components/Transcripts.tsx
+++ frontend/src/components/Transcripts.tsx
@@ -1,125 +1,25 @@
-import moment from "moment"
 import React, { Component } from "react"
-import { Link } from "react-router-dom"
-import { Step } from "../enums"
-import { database } from "../firebaseApp"
-import { ITranscript } from "../interfaces"
-import Progress from "./Progress"
+import TranscriptsList from "./TranscriptsList"
 import Upload from "./Upload"
 
 interface IProps {
   user?: firebase.User
 }
 
-interface IState {
-  transcripts?: ITranscript[]
-  transcriptIds?: string[]
-}
-
-class Transcripts extends Component<IProps, IState> {
-  constructor(props: IProps) {
-    super(props)
-    this.state = {}
-  }
-
-  public componentDidMount() {
-    // Check if have the user in props
-    if (this.props.user !== undefined) {
-      this.fetchTranscripts(this.props.user.uid)
-    }
-  }
-
-  public componentDidUpdate(prevProps, prevState, snapshot) {
-    if (this.props.user !== undefined && this.state.transcripts === undefined) {
-      this.fetchTranscripts(this.props.user.uid)
-    }
-  }
-
+class Transcripts extends Component<IProps> {
   public render() {
     return (
       <main id="transcripts">
-        <div className="transcripts">
-          <h2 className="org-text-xl">Transkripsjoner</h2>
-
-          {/* Render transcripts in progress */}
-          {this.state.transcripts &&
-            this.state.transcripts
-              .filter(transcript => transcript.process.step !== Step.Done)
-              .map((transcript, index) => {
-                return <Progress transcript={transcript} key={index} />
-              })}
-          <table className="org-table">
-            <thead>
-              <tr>
-                <th>Navn</th>
-                <th>Dato</th>
-                <th>Lengde</th>
-              </tr>
-            </thead>
-            <tbody>
-              {/* Render finished transcripts */}
-              {this.state.transcripts !== undefined &&
-                this.state.transcriptIds !== undefined &&
-                this.state.transcripts
-                  .filter(transcript => transcript.process.step === Step.Done)
-                  .map((transcript, index) => {
-                    const createdAt = (transcript.createdAt as firebase.firestore.Timestamp).toDate()
-                    const formattedCreatedAt = moment(createdAt)
-                      .locale("nb")
-                      .calendar()
-                    const id = this.state.transcriptIds[index]
-                    const duration = moment.duration(transcript.metadata.audioDuration * 1000)
-
-                    return (
-                      <tr key={id}>
-                        <td>
-                          <Link to={`transcripts/${id}`}>{transcript.name} </Link>
-                        </td>
-                        <td>{formattedCreatedAt}</td>
-                        <td>
-                          {duration.hours() > 0 ? `${duration.hours()} t ` : ""}
-                          {duration.minutes() > 0 ? `${duration.minutes()} m` : ""}
-                          {duration.hours() === 0 && duration.minutes() === 0 ? `${duration.seconds()} s` : ""}
-                        </td>
-                      </tr>
-                    )
-                  })}
-            </tbody>
-          </table>
-        </div>
-
-        <Upload user={this.props.user} />
+        {this.props.user ? (
+          <>
+            <TranscriptsList userId={this.props.user.uid} />
+            <Upload user={this.props.user} />
+          </>
+        ) : (
+          <div>Loading</div>
+        )}
       </main>
     )
-  }
-
-  private fetchTranscripts(uid: string) {
-    database
-      .collection("/transcripts")
-      .where("userId", "==", uid)
-      .orderBy("createdAt", "desc")
-      .onSnapshot(querySnapshot => {
-        const transcripts = Array<ITranscript>()
-        const transcriptIds = Array<string>()
-
-        querySnapshot.forEach(doc => {
-          // doc.data() is never undefined for query doc snapshots
-          const transcript = doc.data() as ITranscript
-
-          transcripts.push(transcript)
-
-          // We only care about the ids of successful transcripts
-
-          if (transcript.process && transcript.process.step === Step.Done) {
-            transcriptIds.push(doc.id)
-          }
-        })
-
-        this.setState({
-          transcriptIds,
-          transcripts,
-        })
-      })
   }
 }
 

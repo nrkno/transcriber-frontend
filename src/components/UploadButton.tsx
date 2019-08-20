8ac05672d515d927cbf6437678f51ff5e21a59c8
Ann K <ann.develops.work@gmail.com>
2019-08-20 14:48:50 +0300

    Add file type checking before processing

--- src/components/UploadButton.tsx
+++ src/components/UploadButton.tsx
@@ -2,8 +2,11 @@ import React, { Component } from "react"
 import Dropzone, { DropFilesEventHandler } from "react-dropzone"
 import ReactGA from "react-ga"
 
+const VALID_MIME_TYPES_RE = /audio\/|video\/|application\/mxf/
+
 interface IProps {
   fileSelected: (file: File) => void
+  showFileTypeErrorMessage: () => void
 }
 
 class UploadButton extends Component<IProps, any> {
@@ -11,7 +14,7 @@ class UploadButton extends Component<IProps, any> {
     const style = { alignContent: "center", borderColor: "rgb(102, 102, 102)", borderRadius: "5px", borderStyle: "dashed", borderWidth: "2px", display: "grid", height: "100px", justifyContent: "center", position: "relative", width: "100%" }
 
     return (
-      <Dropzone accept="audio/*,video/*,application/mxf" onDrop={this.handleFileDrop}>
+      <Dropzone onDrop={this.handleFileDrop}>
         {({ getRootProps, getInputProps, isDragActive }) => {
           return (
             <div {...getRootProps()} style={style}>
@@ -37,10 +40,13 @@ class UploadButton extends Component<IProps, any> {
         label: rejected[0].type,
       })
     } else {
-      // Take the first file
-      const [file] = accepted
-
-      this.props.fileSelected(file)
+      const filesAreValid = accepted.every(file => VALID_MIME_TYPES_RE.test(file.type))
+      if (filesAreValid) {
+        const [firstFile] = accepted;
+        this.props.fileSelected(firstFile)
+      } else {
+        this.props.showFileTypeErrorMessage()
+      }
     }
   }
 }
